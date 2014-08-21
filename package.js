c28e71e63d1e8443f95049f0664151569415cc82
dan <danphi@gmail.com>
2014-08-20 21:09:57 -0700

    initial

---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+	summary: "Minimap package for Dominus.  http://dominusgame.net",
+	version: "1.0.0",
+	git: "https://github.com/dan335/dominus-minimap.git"
+});
+
+Package.onUse(function(api) {
+	api.versionsFrom('METEOR-CORE@0.9.0-rc10')
+	api.use(['templating', 'deps'], 'client')
+	api.addFiles('server/publish.js', 'server')
+	api.addFiles('client/startup.js', 'client')
+	api.addFiles('client/minimap.css', 'client')
+	api.addFiles('client/minimap.html', 'client')
+	api.addFiles('client/minimap.js', 'client')
+});
